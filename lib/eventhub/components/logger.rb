b9be1da8d270d2c0395b340391fb5c457388b8c0
Pascal Betz <pascal.betz@simplificator.com>
2014-11-07 16:09:37 +0100

    added specs

--- lib/eventhub/components/logger.rb
+++ lib/eventhub/components/logger.rb
@@ -1,5 +1,8 @@
 require 'socket'
 require 'thread'
+
+# A wrapper for loggers to enrich the log message with structured data (a Hash).
+# All methods besides debug/info/warn/error/fatal are forwarded to the target by the means of method_missing.
 class Eventhub::Components::Logger < BasicObject
   SEVERITY_DEBUG = 7
   SEVERITY_INFO = 6
@@ -7,14 +10,22 @@ class Eventhub::Components::Logger < BasicObject
   SEVERITY_ERROR = 3
   SEVERITY_FATAL = 0
 
-  attr_reader :target, :app_name, :pid, :hostname, :env
-
-  def initialize(target, app_name, env)
+  attr_reader :target, :options
+  # Create a new Logger
+  # target: an onject which behaves like a ruby logger.
+  # options (keys are Strings):
+  # * app_name: String, mandatory
+  # * env: String, mandatory
+  # * pid: Number, optional, defaults to Process.pid
+  # * hostname, String, optional, defaults to Socket.gethostname
+  # additional keys are possible and will be sent along. The keys :data and :message are reserved and must not be used.
+  #
+  def initialize(target, options)
     @target = target
-    @app_name = app_name
-    @pid = ::Process.pid
-    @hostname = ::Socket.gethostname
-    @env = env
+    @options = options
+    @options['pid'] ||= ::Process.pid
+    @options['hostname'] ||= ::Socket.gethostname
+    verify_options!
   end
 
   def debug(message, structured_data = nil)
@@ -45,14 +56,19 @@ class Eventhub::Components::Logger < BasicObject
   end
 
   def build_message(severity, message, structured_data)
-    {
+    options.merge({
       'severity' => severity,
       'data' => structured_data,
-      'pid' => pid,
-      'host' => hostname,
-      'message' => message,
-      'env' => env
-    }
+      'message' => message
+    })
+  end
+
+  def verify_options!
+    raise ::ArgumentError.new('target must not be nil') if target.nil?
+    raise ::ArgumentError.new('data is a reserved key') if options.has_key?('data')
+    raise ::ArgumentError.new('message is a reserved key') if options.has_key?('message')
+    raise ::ArgumentError.new('app_name is required') if !options['app_name']
+    raise ::ArgumentError.new('env is required') if !options['env']
   end
 
 end
---
+++ spec/components/logger_spec.rb
@@ -0,0 +1,81 @@
+require_relative '../spec_helper'
+
+RSpec.describe Eventhub::Components::Logger do
+  context 'initalize' do
+    it 'no raise when all required arguments are given' do
+      expect {
+        Eventhub::Components::Logger.new("something not nil", 'app_name' => 'an app', 'env' => 'test')
+      }.to_not raise_error
+    end
+    it 'requires app_name' do
+      expect {
+        Eventhub::Components::Logger.new("something not nil", 'env' => 'test')
+      }.to raise_error(ArgumentError)
+    end
+    it 'requires app_name' do
+      expect {
+        Eventhub::Components::Logger.new("something not nil", 'app_name' => 'an app')
+      }.to raise_error(ArgumentError)
+    end
+
+    it 'adds the default options' do
+      logger = Eventhub::Components::Logger.new("something not nil", 'app_name' => 'an app', 'env' => 'test')
+      expect(logger.options['pid']).to eq(::Process.pid)
+      expect(logger.options['hostname']).to eq(::Socket.gethostname)
+    end
+  end
+
+  context 'proxy' do
+    let(:logger) do
+      # fake logger that stores a log message
+      # in hash
+      fake_logger = Hash.new
+      def l.debug(message); self[:debug] = message; end
+      def l.info(message); self[:info] = message; end
+      def l.warn(message); self[:warn] = message; end
+      def l.error(message); self[:error] = message; end
+      def l.fatal(message); self[:fatal] = message; end
+      fake_logger
+    end
+    subject {
+      Eventhub::Components::Logger.new(logger, 'app_name' => 'an app', 'env' => 'test')
+    }
+
+    it 'forwards calls to the target when not related to log methods' do
+      expect(subject.class).to eq(logger.class)
+    end
+
+    it 'enriches and forwards debug calls' do
+      subject.debug("my message debug", {'some' => 'debug'})
+      expectation = {'app_name' => 'an app', 'data' => {'some' => 'debug'}, 'env' => 'test', 'hostname' => Socket.gethostname, 'message' => 'my message debug', 'pid' => Process.pid, 'severity' => 7}
+      expect(logger[:debug]).to eq(expectation)
+    end
+
+    it 'enriches and forwards info calls' do
+      subject.info("my message info", {'some' => 'info'})
+      expectation = {'app_name' => 'an app', 'data' => {'some' => 'info'}, 'env' => 'test', 'hostname' => Socket.gethostname, 'message' => 'my message info', 'pid' => Process.pid, 'severity' => 6}
+      expect(logger[:info]).to eq(expectation)
+    end
+
+    it 'enriches and forwards warn calls' do
+      subject.warn("my message warn", {'some' => 'warn'})
+      expectation = {'app_name' => 'an app', 'data' => {'some' => 'warn'}, 'env' => 'test', 'hostname' => Socket.gethostname, 'message' => 'my message warn', 'pid' => Process.pid, 'severity' => 4}
+      expect(logger[:warn]).to eq(expectation)
+    end
+
+    it 'enriches and forwards error calls' do
+      subject.error("my message error", {'some' => 'error'})
+      expectation = {'app_name' => 'an app', 'data' => {'some' => 'error'}, 'env' => 'test', 'hostname' => Socket.gethostname, 'message' => 'my message error', 'pid' => Process.pid, 'severity' => 3}
+      expect(logger[:error]).to eq(expectation)
+    end
+
+    it 'enriches and forwards fatal calls' do
+      subject.fatal("my message fatal", {'some' => 'fatal'})
+      expectation = {'app_name' => 'an app', 'data' => {'some' => 'fatal'}, 'env' => 'test', 'hostname' => Socket.gethostname, 'message' => 'my message fatal', 'pid' => Process.pid, 'severity' => 0}
+      expect(logger[:fatal]).to eq(expectation)
+    end
+
+  end
+
+
+end
