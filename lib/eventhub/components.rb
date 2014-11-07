518986bbbe199b56a6cd7137a7b0c30e2f914651
Pascal Betz <pascal.betz@simplificator.com>
2014-11-07 10:14:30 +0100

    initial

---
+++ lib/eventhub/components.rb
@@ -0,0 +1,7 @@
+require "eventhub/components/version"
+
+module Eventhub
+  module Components
+    # Your code goes here...
+  end
+end
---
+++ lib/eventhub/components/version.rb
@@ -0,0 +1,5 @@
+module Eventhub
+  module Components
+    VERSION = "0.0.1"
+  end
+end
